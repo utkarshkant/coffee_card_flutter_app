b7d2aa9de9696a30d87cd82cbd4f8c46b27143e7
Utkarsh Kant <utkarsh.kant@gmail.com>
2025-08-25 16:24:05 +0530

    first commit for coffee card flutter app

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Sandbox()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(height: 100, color: Colors.red, child: const Text('one')),
+          Container(height: 200, color: Colors.green, child: const Text('two')),
+          Container(
+            height: 300,
+            color: Colors.blue,
+            child: const Text('three'),
+          ),
+        ],
+      ),
+    );
+  }
+}
